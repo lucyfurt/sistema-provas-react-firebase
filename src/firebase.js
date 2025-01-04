54872a6f4bda2dfe68bc622b658c12f44913d368
lucyfurt <lucianafurtado10@gmail.com>
2025-01-04 19:39:50 -0300

    change gitignore

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyBmf1T7T9bF7yX5bGW2iogfzDrOFyVv88U",
-  authDomain: "sistema-de-avaliacao-a90c8.firebaseapp.com",
-  projectId: "sistema-de-avaliacao-a90c8",
-  storageBucket: "sistema-de-avaliacao-a90c8.firebasestorage.app",
-  messagingSenderId: "270757295487",
-  appId: "1:270757295487:web:9c203d4ef90644dc85964a"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
+
 // Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
 
